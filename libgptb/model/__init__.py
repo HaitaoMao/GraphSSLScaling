0030b9a7bb10697654f3903d153802a528086ba3
HaitaoMao <haitaoma@msu.edu>
2024-10-23 14:09:14 -0400

    Update Singularity.def

--- libgptb/model/__init__.py
+++ libgptb/model/__init__.py
@@ -7,7 +7,6 @@ from libgptb.model.GBT import GBT
 from libgptb.model.GRACE import GRACE
 from libgptb.model.MVGRL import MVGRL
 from libgptb.model.COSTA import COSTA
-from libgptb.model.HomoGCL import HomoGCL
 from libgptb.model.GraphCL import GraphCL
 from libgptb.model.MVGRLg import MVGRLg
 from libgptb.model.InfoGraph import InfoGraph
@@ -25,7 +24,6 @@ __all__ = [
     GRACE,
     MVGRL,
     COSTA,
-    HomoGCL,
     GraphCL,
     MVGRLg,
     InfoGraph,
